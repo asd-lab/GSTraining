e695000a688c770df175ae03a9916df5b3962077
Adarsh Dubey <55585713+asd-lab@users.noreply.github.com>
2020-06-22 10:43:57 +0530

    Update Angrams.java

--- Angrams.java
+++ Angrams.java
@@ -1,41 +1,23 @@
 package misc;
 
 import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
-public class Angrams {
+public class FindAnagram {
+
+	public static void main(String[] args) {
+		
+
+	String[] input = {"cat","dog","tac","god","act"};
+	List<String> list = Arrays.asList(input);
 	
-	static final int SIZE = 256; 
-	public static boolean isAnagram(String str1, String str2) {
-		
-		int[] count = new int[SIZE];
-		int i;
-		
-		for(i = 0; i < str1.length() && i < str2.length(); i++) {
-			count[str1.charAt(i)]++; 
-			count[str2.charAt(i)]--; 
-		}
-		
-		 if (str1.length() != str2.length()) 
-	          return false; 
-	  
-		 for (i = 0; i < SIZE; i++) 
-	            if (count[i] != 0) 
-	                return false; 
-	         return true; 
+	Map<String,List<String>> anagram = list.stream().collect(Collectors.groupingBy(str -> {char[] c = 
+										str.toCharArray();
+										Arrays.sort(c);
+										return new String(c);
+										}));
+	anagram.forEach((k,v)->System.out.println(v));;
 	}
-	
-	static void findAllAnagrams(String arr[], int n) 
-    { 
-        for (int i = 0; i < n; i++) 
-            for (int j = i+1; j < n; j++) 
-                if (isAnagram(arr[i], arr[j])) 
-                    System.out.print("["+arr[i] +","+ arr[j]+"]"); 
-    }
-	public static void main(String args[]) 
-    { 
-        String arr[] = {"cat","dog", "tac","god", "act"}; 
-        Arrays.sort(arr);
-        int n = arr.length; 
-        findAllAnagrams(arr, n); 
-    } 
 }
