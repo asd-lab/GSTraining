8af95c88812a0425f9b63ab6aa6afc707fea50ba
Adarsh Dubey <55585713+asd-lab@users.noreply.github.com>
2020-07-31 16:51:18 +0530

    Update ShiftZeros.java

--- ShiftZeros.java
+++ ShiftZeros.java
@@ -1,24 +1,22 @@
-
 public class ShiftZeros {
 	
-	public static int[] shiftZeros(int arr[] ) {
+	public static int[] shiftZeros(int[] arr ) {
 		
-		int j = 0 , temp;
+		int j = arr.length-1 , temp;
 		
-		for(int i = 0 ; i < arr.length; i++) {
-			if(arr[i] != 0) {
+		for(int i = arr.length-1 ; i >= 0; i--) {
+			if(arr[i] != 0) { 
 				temp = arr[j];
 				arr[j] = arr[i];
 				arr[i] = temp;
-				j++;
+				j--;
 			}
 		}
 		
 		return arr;
-		
 	}
-	public static void printArray(int arr[]) {
-		for(int x : arr) {
+	public static void printArray(int[] array) {
+		for(int x : array) {
 			System.out.print(x+ " ");
 		}
 	}
@@ -26,8 +24,8 @@ public class ShiftZeros {
 	
 	
 	public static void main(String[] args) {
-		int arr[] = {1,0,2,0,3,0};
-		int array[] = shiftZeros(arr);
+		int[] arr = {0,1,0,2,0,3,0,5,4,0,1,0};
+		int[] array = shiftZeros(arr);
 		printArray(array);
 		
 	}
